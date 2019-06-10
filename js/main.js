b0e322080572ce8e83135e0c1c272a140651a2fd
Victor <vale1c@ufrn.edu.br>
2019-06-09 23:48:33 -0300

    todo: footer

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
  (function($){
-    
-    $('.paroller').paroller()
-    var contentWaypoint = function(){
-        $('.element-animate').waypoint( function( direction ){
-            console.log(direction)
-            console.log(this)
-            console.log(this.element)
-            
-            const $element = $(this.element);
-            let effect = $element.data('animate-effect') || 'fadeInUp';
-                        
-            
-            
-            if(direction === 'down' && !$element.hasClass('element-animated')){
-                $element.removeClass('element-animate').addClass('element-animated ' + effect)
-            }
-        }, {
-            offset: '90%'
-        })
-    }
-    
-    contentWaypoint()
+  
+  $('.paroller').paroller()
+  var contentWaypoint = function(){
+    $('.element-animate').waypoint( function( direction ){
+      console.log(direction)
+      console.log(this)
+      console.log(this.element)
+      
+      const $element = $(this.element);
+      let effect = $element.data('animate-effect') || 'fadeInUp';
+      
+      
+      
+      if(direction === 'down' && !$element.hasClass('element-animated')){
+        $element.removeClass('element-animate').addClass('element-animated ' + effect)
+      }
+
+    }, {
+      offset: '90%'
+    })
+  }
+  
+  contentWaypoint()
+  
 })(jQuery)
 
 function openModal() {
@@ -48,14 +50,14 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+    if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+      captionText.innerHTML = dots[slideIndex-1].alt;
+    }
